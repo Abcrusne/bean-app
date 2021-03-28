e27ceca0cab528bc012920e0b92898cc5c4b63e7
Abcrusne <rusne.pet@gmail.com>
2021-03-28 15:36:23 +0300

    Vaiku darzeliu informacine sistema

---
+++ Darzelio-Back/bean-app/src/main/resources/public/precache-manifest.8773a9a00a5fd419bf17bae64eb7f9d3.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d27470c5504d679432f63c22266392",
+    "url": "/bean-app/index.html"
+  },
+  {
+    "revision": "ae783ca0a0663e6103ce",
+    "url": "/bean-app/static/css/4.eb6b45d6.chunk.css"
+  },
+  {
+    "revision": "56d73a73ca9558427628",
+    "url": "/bean-app/static/css/main.6b65511c.chunk.css"
+  },
+  {
+    "revision": "8e0fc8798578d9035a22",
+    "url": "/bean-app/static/js/0.0392fbba.chunk.js"
+  },
+  {
+    "revision": "771690d07476505ecdcb",
+    "url": "/bean-app/static/js/1.c821f1bc.chunk.js"
+  },
+  {
+    "revision": "a53a9ba4e38ea6e91ae4",
+    "url": "/bean-app/static/js/10.c29708e4.chunk.js"
+  },
+  {
+    "revision": "dcb035ec375b4fa9504b",
+    "url": "/bean-app/static/js/11.f15d4968.chunk.js"
+  },
+  {
+    "revision": "676bcb719dd9d614390b",
+    "url": "/bean-app/static/js/12.e6e413bc.chunk.js"
+  },
+  {
+    "revision": "bedc143e1da16b3766be",
+    "url": "/bean-app/static/js/13.8b687752.chunk.js"
+  },
+  {
+    "revision": "573b3e2116a2c2d24232",
+    "url": "/bean-app/static/js/14.780c833c.chunk.js"
+  },
+  {
+    "revision": "df8fa09399f51b277c44",
+    "url": "/bean-app/static/js/15.f9841348.chunk.js"
+  },
+  {
+    "revision": "e4b59898f32eb5cd887f",
+    "url": "/bean-app/static/js/16.195b5d77.chunk.js"
+  },
+  {
+    "revision": "458368b1692e231a6d16",
+    "url": "/bean-app/static/js/17.f3d6532a.chunk.js"
+  },
+  {
+    "revision": "dea3f9f5fdc71dd4c9f9",
+    "url": "/bean-app/static/js/18.66420aeb.chunk.js"
+  },
+  {
+    "revision": "c665d8316cd42c3f3482",
+    "url": "/bean-app/static/js/19.02bb4fb2.chunk.js"
+  },
+  {
+    "revision": "0dd5d261d661daadf202",
+    "url": "/bean-app/static/js/20.ce3ab89f.chunk.js"
+  },
+  {
+    "revision": "3e1d64cda162308add11",
+    "url": "/bean-app/static/js/21.b3fb765b.chunk.js"
+  },
+  {
+    "revision": "a7cd2eaa75bfbc1b170f",
+    "url": "/bean-app/static/js/22.5a1f8474.chunk.js"
+  },
+  {
+    "revision": "1612713d7cd63a503bd6",
+    "url": "/bean-app/static/js/23.cf809f27.chunk.js"
+  },
+  {
+    "revision": "c23d32b4e8f655d05a8e",
+    "url": "/bean-app/static/js/24.0cc712b4.chunk.js"
+  },
+  {
+    "revision": "8bc592884d8f1b982b81",
+    "url": "/bean-app/static/js/25.da4fc25c.chunk.js"
+  },
+  {
+    "revision": "712ba584e44f54d07a5f",
+    "url": "/bean-app/static/js/26.80eeefcf.chunk.js"
+  },
+  {
+    "revision": "ae783ca0a0663e6103ce",
+    "url": "/bean-app/static/js/4.7da827f4.chunk.js"
+  },
+  {
+    "revision": "e914879a7b5efc0296af7a2ee3a7dc5e",
+    "url": "/bean-app/static/js/4.7da827f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111fad1788e5a2ed8784",
+    "url": "/bean-app/static/js/5.f452e335.chunk.js"
+  },
+  {
+    "revision": "22ac3c55d0fa2a11db2c",
+    "url": "/bean-app/static/js/6.f172a4f2.chunk.js"
+  },
+  {
+    "revision": "a41f15d6b6de66cee03d",
+    "url": "/bean-app/static/js/7.bdb8bffc.chunk.js"
+  },
+  {
+    "revision": "fbfaf2ddf17ff8df9bfd",
+    "url": "/bean-app/static/js/8.f0a577cb.chunk.js"
+  },
+  {
+    "revision": "a1d2081b1d102c71649a",
+    "url": "/bean-app/static/js/9.75947ccf.chunk.js"
+  },
+  {
+    "revision": "56d73a73ca9558427628",
+    "url": "/bean-app/static/js/main.e1393ef9.chunk.js"
+  },
+  {
+    "revision": "e139945e777869ac5b9d",
+    "url": "/bean-app/static/js/runtime-main.f29604a9.js"
+  },
+  {
+    "revision": "91694149cb1d2686ede9005b128f467e",
+    "url": "/bean-app/static/media/Edu.91694149.pdf"
+  },
+  {
+    "revision": "3888b0189b213617336c65e14eeacbdd",
+    "url": "/bean-app/static/media/SysAdmin.3888b018.pdf"
+  },
+  {
+    "revision": "33b55f495eb4e7a47e2ba2427d283210",
+    "url": "/bean-app/static/media/Tevai.33b55f49.pdf"
+  }
+]);
